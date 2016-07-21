b202f647998a03a51ab5f39399a88caa65be494d
kyle zemek <kyle@wealthfront.com>
2016-07-20 19:22:26 -0700

    test

--- example.py
+++ example.py
@@ -910,4 +910,5 @@ if __name__ == '__main__':
     print "I ran!"
     args = get_args()
     register_background_thread(initial_registration=True)
-    app.run(debug=True, threaded=True, host=args.host, port=args.port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, threaded=True, host=args.host, port=port)
